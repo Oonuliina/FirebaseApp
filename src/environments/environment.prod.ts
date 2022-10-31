6d8a6f0219ddaf36498a9ee3732328f12600f69b
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2022-10-31 14:45:12 +0200

    second commit
    for some reason the first commit didn't bring all the files to github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-app-9d2a1',
+    appId: '1:1089236245237:web:4dfa9f50bdb41f235603bc',
+    storageBucket: 'fir-app-9d2a1.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDQuiWyG_uXSqJvebuiVR5WY6NdPychlMQ',
+    authDomain: 'fir-app-9d2a1.firebaseapp.com',
+    messagingSenderId: '1089236245237',
+  },
   production: true
 };
